b29900b154c6e36152137d0ddab4ead002dcf2f4
butcherzn <helloworld_zhang@163.com>
2014-10-19 13:08:10 +0800

    1.1

---
+++ src/com/huyeye/weather/activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.huyeye.weather.activity;
+
+import com.huyeye.weather.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_splash);
+
+	}
+}
